934ce0f08b3bda8e207c6a5855f210f7028141af
pradeepappala <pradeep.appala@gmail.com>
2021-01-27 16:31:42 +0530

    Heapify, Merge Sort

--- examples/bSearch.py
+++ examples/bSearch.py
@@ -1,11 +1,122 @@
-def bSearch(a, k):
+def bSearch_mod(a, k):
     if not a:
-        return False
+        return -1
 
     if a[len(a)//2] == k:
-        return True
+        return len(a)//2
 
     if a[len(a)//2] < k:
-        return bSearch(a[len(a)//2+1:], k)
+        import math
+        return math.ceil(len(a)/2) + bSearch(a[len(a)//2+1:], k)
 
     return bSearch(a[:len(a)//2], k)
+
+
+def bSearch(a, l, h, k):
+    if not a or l > h:
+        return -1
+
+    m = (l + h)//2
+
+    if a[m] == k:
+        return m
+
+    if a[m] < k:
+        return bSearch(a, m+1, h, k)
+
+    return bSearch(a, l, m - 1, k)
+
+
+def myHeapify(a, i):
+    c1i = (i+1)*2-1
+    c2i = (i+1)*2
+    v1 = v2 = None
+    if c1i < len(a):
+        v1 = a[c1i]
+    if c2i < len(a):
+        v2 = a[c2i]
+
+    if not v1 and not v2:
+        return  # leaf node
+
+    if v1 and not v2:
+        if v1 <= a[i]:
+            return  # already heap
+        a[c1i], a[i] = a[i], a[c1i]
+        return  # heap is a complete binary tree, this case occurs only once and no need to proceed further
+
+    if v1 <= a[i] and v2 <= a[i]:
+        return  # already heap
+
+    if v1 < v2:
+        a[c2i], a[i] = a[i], a[c2i]
+        return myHeapify(a, c2i)  # swap and run heapify on child
+
+    a[c1i], a[i] = a[i], a[c1i]
+    return myHeapify(a, c1i)  # swap and run heapify on child
+
+
+def mergeSort(a, l, h):
+    if l >= h:
+        return
+
+    m = (l + h) // 2
+    mergeSort(a, l, m)
+    mergeSort(a, m+1, h)
+    merge(a, l, m, h)
+
+
+def merge(a, l, m, h):
+    res = [-1] * (h -l + 1)
+    k = 0
+    i = l
+    j = m + 1
+    while i <= m and j <= h:
+        if a[i] <= a[j]:
+            res[k] = a[i]
+            i += 1
+            k += 1
+        else:
+            res[k] = a[j]
+            j += 1
+            k += 1
+
+    if i <= m:
+        for v in a[i:m+1]:
+            res[k] = v
+            k += 1
+
+    if j <= h:
+        for v in a[j:h+1]:
+            res[k] = v
+            k += 1
+
+    a[l:h+1] = res
+
+
+if __name__ == '__main__':
+    print(bSearch([1, 2, 3, 4], 0, 4, 1))
+    print(bSearch([1, 2, 3, 4], 0, 4, 2))
+    print(bSearch([1, 2, 3, 4], 0, 4, 3))
+    print(bSearch([1, 2, 3, 4], 0, 4, 4))
+    print(bSearch([1, 2, 3, 4, 5], 0, 5, 4))
+    print(bSearch([1, 2, 3, 4, 5], 0, 5, 5))
+    a = [1, 2, 3, 4, 5, 6]
+    print(a)
+    for i in range(len(a), 0, -1):
+        myHeapify(a, i - 1)
+        print(f'After heapify at {i - 1} is {a}')
+    print(a)
+
+    a = [5, 2, 3, 1, 4]
+    c = [-1] * len(a)
+    merge(a, 1, 2, 4)
+    print(c)
+    print(a)
+
+    a = [3, 2, 1]
+    c = [-1] * len(a)
+    mergeSort(a, 0, len(a) - 1)
+    print(c)
+    print(a)
+
--- examples/test_bSearch.py
+++ examples/test_bSearch.py
@@ -4,14 +4,38 @@ import bSearch
 
 class Test_bSearch(unittest.TestCase):
     def test_founds(self):
-        self.assertTrue(bSearch.bSearch([1, 2, 3], 1))
-        self.assertTrue(bSearch.bSearch([1, 2, 3], 2))
-        self.assertTrue(bSearch.bSearch([1, 2, 3], 3))
-        self.assertTrue(bSearch.bSearch([1, 2, 3, 4], 2))
-        self.assertTrue(bSearch.bSearch([1, 2, 3, 4, 5, 6, 7], 5))
+        self.assertEqual(bSearch.bSearch([1, 2, 3], 0, 2, 1), 0)
+        self.assertEqual(bSearch.bSearch([1, 2, 3], 0, 2, 2), 1)
+        self.assertEqual(bSearch.bSearch([1, 2, 3], 0, 2, 3), 2)
+        self.assertEqual(bSearch.bSearch([1, 2, 3, 4], 0, 3, 2), 1)
+        self.assertEqual(bSearch.bSearch([1, 2, 3, 4, 5, 6, 7], 0, 6, 5), 4)
 
     def test_notfound(self):
-        self.assertFalse(bSearch.bSearch([1, 2, 3], 4))
+        self.assertEqual(bSearch.bSearch([1, 2, 3], 0, 2, 4), -1)
+        self.assertEqual(bSearch.bSearch([], 0, 0, 4), -1)
+
+
+class Test_myHeapify(unittest.TestCase):
+    def test_my_heapify(self):
+        a = [1, 2, 3, 4, 5]
+        bSearch.myHeapify(a, 4)
+        self.assertEqual(a, [1, 2, 3, 4, 5])
+        bSearch.myHeapify(a, 1)
+        self.assertEqual(a, [1, 5, 3, 4, 2])
+        a = [1, 2, 3, 4, 5]
+
+
+class Test_merge(unittest.TestCase):
+    def setUp(self):
+        self.a = [2, 1, 3, 4, 5]
+
+    def test_merge(self):
+        bSearch.merge(self.a, 0, 0, 1)
+        self.assertEqual(self.a, [1, 2, 3, 4, 5])
+
+    def test_mergeSort(self):
+        bSearch.mergeSort(self.a, 0, len(self.a) - 1)
+        self.assertEqual(self.a, [1, 2, 3, 4, 5])
 
 
 if __name__ == '__main__':
